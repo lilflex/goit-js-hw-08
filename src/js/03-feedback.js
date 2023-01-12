5de2835a3ef18d5a64f5aa9489eab5f8a24c465c
Aleksej <kralinaleksej@gmail.com>
2023-01-12 16:04:28 +0200

    HW08

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,7 +4,6 @@ const form = document.querySelector('.feedback-form');
 const email = document.querySelector('input[name="email"]');
 const message = document.querySelector('textarea[name="message"]');
 const LOCALSTORAGE_KEY = 'feedback-form-state';
-console.log(throttle);
 form.addEventListener(
   'input',
   throttle(e => {
